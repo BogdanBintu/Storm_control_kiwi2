34260df92c9d497b2cd0d3caf0a595bc39c282f8
Y.Han <yuanyuankathyhan@gmail.com>
2019-08-07 13:36:09 -0700

    First commit

---
+++ storm_control/fluidics/scope_settings/kilroy_storm65.bat
@@ -0,0 +1,1 @@
+C:\Anaconda3\Scripts\activate testStorm&&C:\Anaconda3\envs\testStorm\python ..\kilroy.py kilroy_settings_storm65.xml
---
+++ storm_control/hal4000/batFiles/storm65.bat
@@ -0,0 +1,1 @@
+path=C:\Anaconda3\envs\testStorm;&&C:\Anaconda3\Scripts\activate testStorm&&python ..\hal4000.py ..\xml\storm65.xml
